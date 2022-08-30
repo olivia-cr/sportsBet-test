2b55b7c87793de8fbd9b68df07641e22d6a0ad5e
olivia-e-c <olivia.c@linktr.ee>
2022-08-30 23:49:42 +1000

    remove empty navigate to

--- cypress/pages/racingHome.ts
+++ cypress/pages/racingHome.ts
@@ -1,11 +1,6 @@
 import { Home } from "./home";
 
 export class RacingHome extends Home {
-    navigateTo(){
-        cy.visit('/racing')
-        return this;
-    }
-
     selectNextToJump(){
         cy.get('div[data-automation-id="group-1-carousel-1-body-container-cell-1"]').click()
         return this;
